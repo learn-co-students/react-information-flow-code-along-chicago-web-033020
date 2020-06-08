789bdd0f471aa841724f216680a44d307c6fe610
github username <github email address>
2020-06-07 20:23:30 -0400

    Done.

--- src/Child.js
+++ src/Child.js
@@ -4,11 +4,13 @@ import { getRandomColor } from './randomColorGenerator.js'
 class Child extends Component {
   render() {
     return (
-      <div
+      <div onClick={() => this.props.handleColorChange(getRandomColor())} //calling handleColorChange from parent component
         className="child"
-        style={{backgroundColor: "#FFF"}}
+        style={{backgroundColor: this.props.color}}
       ></div>
-    )
+    ) // style={{backgroundColor: "#FFF"}} ---- original line 9
+    // change to the prop data we set in parent.
+  // go change Child color from #FFF to newChildColor, to use as argument in changeColor
   }
 }
 
--- src/Parent.js
+++ src/Parent.js
@@ -7,17 +7,31 @@ class Parent extends Component {
   constructor() {
     super()
     this.state = {
-      color: getRandomColor()
+      color: getRandomColor(),
+      childrenColor: '#FFF'
     }
   }
+
+  changeColor = (newChildColor) => {
+    this.setState({
+      color: getRandomColor(),
+                              // need to pass this color to the other child,
+      childrenColor: newChildColor // but cant pass data between siblings,
+                          // so we store the color of the child in this parent component
+                        // and have Parent do the passing, seen in lines 29 and 30
+    })
+  }
+
+  //passing handleColorChange function as a PROP to child components
 
   render() {
     return (
       <div className="parent" style={{backgroundColor: this.state.color}}>
-        <Child />
-        <Child />
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor} />
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor} />
       </div>
-    )
+    ) // now, we go use this prop data (lines 29 & 30)
+    // in our child component, to change the backgorund color
   }
 }
 
